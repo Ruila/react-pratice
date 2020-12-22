0eab7f29d9a23851e7ff262638451ba38487a2a3
mengru <4momolmr@gmail.com>
2020-12-23 07:37:38 +0800

    loop

--- hello-world/src/api/youtubeApi.js
+++ hello-world/src/api/youtubeApi.js
@@ -3,7 +3,7 @@ import _axios from "axios"
 const axios = (baseURL) => {
     //建立一個自定義的axios
     const instance = _axios.create({
-            baseURL: "https://www.googleapis.com/youtube/v3/channels?part=contentDetails,snippet&id=UCMUnInmOkrWN4gof9KlhNmQ&key=AIzaSyDTYKiAZ33YnLej2kOuVPkckWz5RVSycJg" || 'http://localhost:3003', //JSON-Server端口位置
+            baseURL: "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&part=contentDetails&playlistId=UU0C-w0YjGpqDXGB8IHb662A&key=AIzaSyDTYKiAZ33YnLej2kOuVPkckWz5RVSycJg" || 'http://localhost:3003', //JSON-Server端口位置
             timeout: 1000,
         });
 
--- hello-world/src/components/player.js
+++ hello-world/src/components/player.js
@@ -1,28 +1,107 @@
 import '../css/style.css';
 import videoimg from '../images/c-mobile-tamou-logo-draw.jpg';
 import Api from "../api/youtubeApi.js";
+import React, {Component} from 'react';
+import { render } from '@testing-library/react';
+import _axios from "axios"
+import axios from 'axios';
 
-function player_block() {
-  try 
-  {
+class player_block extends Component {
+    constructor() {
+      super();
+      this.state = {
+        lala: [],
+        debug: ''
+      }
+      this.getApi=this.getApi.bind(this);
+    }
       //取得數據庫http://localhost:3003/posts的數據
-      const Data =  Api.get(); 
-      console.log(Data);
-  } 
-  catch (error) 
-  {
-      alert("GET Error!!");    
-  } 
-  return (
-    <div className="player-block">
-        <div className="img">
-            <img src={videoimg} alt="videoimg"/>
-        </div>
-        <div className="text">
-            <span>videoName</span>
-        </div>
-    </div>
-  );
+      getApi() {
+        Api.get().then((v)=>{
+            // videoList = v.data.items
+            this.setState({lala: v.data.items})
+            // debug["url"] = v.data.items.snippet.thumbnails.medium.url
+            console.log(v.data.items);
+         
+            
+            // console.log('success')
+        }).catch(() => {
+        console.log('failure');
+        })
+
+        // console.log(this.state.lala);
+
+      }
+
+      componentDidMount(){
+        let list = []
+        // axios.get('https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&part=contentDetails&playlistId=UU0C-w0YjGpqDXGB8IHb662A&key=AIzaSyDTYKiAZ33YnLej2kOuVPkckWz5RVSycJg')
+        // .then((v)=>{
+        //   console.log('did', v)
+        // })
+        axios.get('https://source.unsplash.com/collection/190727/1600x900')
+        .then((v)=>{
+          for(let i=0; i<5; i++){
+            list[i] = `${v.config.url}`;
+          }
+          this.setState({debug: v.config.url})
+          this.setState({lala: list})
+        })
+      }
+      
+      render(){
+        // console.log('did', this.state.debug)
+
+        // console.log('did', this.state.lala.length)
+        let lists = this.state.lala.map((list, index) => {
+            <div  className="player-block">
+            <div className="img">
+              <img key={index} src={list} alt="videoimg"/>
+            </div>
+              <div className="text">
+                  <span>videoName</span>
+              </div>
+            </div>
+        })
+        console.log('?????', lists)
+        if(this.state.lala.length == 5){
+          console.log('good')
+          // console.log(lists)
+          return (
+            <div>
+                {lists}
+            </div>
+              
+              // <div  className="player-block">
+              //   <div className="img">
+              //     <img src={videoimg} alt="videoimg"/>
+              //   </div>
+              //   <div className="text">
+              //       <span>videoName</span>
+              //   </div>
+              //  </div>
+            );
+        }else{
+          console.log('gGG')
+          console.log('ffff',lists)
+          return (
+            <div>
+                {lists}
+            </div>
+            // <div  className="player-block">
+              //   <div className="img">
+              //     <img src={videoimg} alt="videoimg"/>
+              //   </div>
+              //   <div className="text">
+              //       <span>videoName</span>
+              //   </div>
+              //  </div>
+            );
+        }
+       
+      }
+ 
+  
 }
 
 export default player_block;
