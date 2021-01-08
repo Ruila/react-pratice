44696b334abb130c0cf142471b46ddfe8961c5bd
mengru <4momolmr@gmail.com>
2021-01-08 16:17:44 +0800

    test is so hard

--- hello-world/src/test/index.test.js
+++ hello-world/src/test/index.test.js
@@ -10,20 +10,31 @@ import Adapter from 'enzyme-adapter-react-16'
 // 以該解析器提供給 Enzyme 做渲染 Component 的設置
 Enzyme.configure({ adapter: new Adapter() })
 
+import configureMockStore from 'redux-mock-store';
+import fetchMock from 'fetch-mock';
 // import 受測 Component 也就是此測試的 SUT
 import Index from "../components/videoBoard.js";
 import Btn from '../components/button.js'
+import Player from '../components/player.js'
 
+//api key
+import {apikey} from "../api/apikey.js";
+
+const middlewares = []
+const mockStore = configureMockStore(middlewares)
 
 
 describe('test search', () => {
+    const initialState = {}
+    const store = mockStore(initialState)
     let videoBoard = shallow(<Index />)
     let button = shallow((<Btn />));
+    let player = shallow((<Player store={store}/>))
     beforeEach(()=>{
       
     })
 
-    test('Check initial state', () => {
+    test('Check initial state in button.js', () => {
       button.find('input').simulate('change', {target: {value: '宜蘭縣'}});
       expect(button.state('inputValue')).toBe("宜蘭縣")
     })
@@ -37,8 +48,9 @@ describe('test search', () => {
        
     });
     
+    test('test api', () => {
+      
+    });
     
-    // fireEvent.click(screen.getByTestId("btn"));
-    // expect(screen.getByRole("data-block")).toBeInTheDocument()
 
 });
